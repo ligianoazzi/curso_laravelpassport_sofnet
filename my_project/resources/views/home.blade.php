e4a1172542e13ce7b4aef9fd0993d089f54b920a
Ligiano <ldaazzi@gmail.com>
2017-08-14 10:36:17 -0300

    agora que os componentes estão integrados, precisamos escolher uma página que requisite estar logado para colocar os componentes, vamos colocar na home

--- my_project/resources/views/home.blade.php
+++ my_project/resources/views/home.blade.php
@@ -13,5 +13,22 @@
             </div>
         </div>
     </div>
+
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <passport-clients></passport-clients>
+                </div>
+                <div class="panel-body">
+                    <passport-authorized-clients></passport-authorized-clients>
+                </div>               
+                <div class="panel-body">
+                    <passport-personal-access-tokens></passport-personal-access-tokens>
+                </div>               
+            </div>
+        </div>
+    </div>
+
 </div>
 @endsection
